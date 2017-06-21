f72ef0c7cecb50604dd1e42f04dd56c232762fcb
elkhann <emilkhann@gmail.com>
2017-06-21 11:07:07 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -68,6 +69,12 @@ class User < ActiveRecord::Base
   # Возвращает true, если время для сброса пароля истекло.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  # Определяет прото-ленту.
+  # Полная реализация приводится в разделе "Следование за пользователями".
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
